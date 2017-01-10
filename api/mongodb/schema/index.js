47ff4b8a01b01b9df912ce7356be68043a2cb55e
Suranart Niamcome <suranartnc@gmail.com>
2017-01-10 18:25:57 +0700

    add categories in post

--- api/mongodb/schema/index.js
+++ api/mongodb/schema/index.js
@@ -1,10 +1,23 @@
 export const typeDefs = [`
+
   type PostType {
     _id: String!
     title: String!
     body: String!
     author: UserType!
+    categories: [CategoryType]
     comments: [CommentType]
+  }
+
+  type CategoryType {
+    title: String!
+    slug: String!
+  }
+
+  type CommentType {
+    body: String!
+    author: UserType!
+    date: String!
   }
 
   type UserType {
@@ -19,24 +32,32 @@ export const typeDefs = [`
     picture: String
   }
 
-  type CommentType {
-    body: String!
-    author: UserType!
-    date: String!
-  }
 `]
 
 export const resolvers = {
+
   PostType: {
+
+    categories(post, args, { CategoryModel }) {
+      return CategoryModel.find({
+        _id: {
+          $in: post.categories,
+        },
+      })
+    },
+
     author(post, args, { UserModel }) {
       return UserModel.findById(post.userId)
     },
+
     comments(post, args, { CommentModel }) {
       return CommentModel.find({
         postId: post._id
       })
     }
+
   },
+
   CommentType: {
     author(comment, args, { UserModel }) {
       return UserModel.findById(comment.userId)
