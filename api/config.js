44be6e7100bf64ca99c6e99842eb543347b23568
Suranart Niamcome <suranartnc@gmail.com>
2017-01-13 16:18:18 +0700

    change port

--- api/config.js
+++ api/config.js
@@ -2,13 +2,13 @@ module.exports = {
   development: {
     isProduction: false,
     host: 'localhost',
-    port: 3001,
+    port: 3002,
     mongoConnectionString: 'mongodb://localhost:27017/blog',
   },
   production: {
     isProduction: true,
     host: 'localhost',
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || 3002,
     mongoConnectionString: 'mongodb://localhost:27017/blog',
   },
 }[process.env.NODE_ENV || 'development']
